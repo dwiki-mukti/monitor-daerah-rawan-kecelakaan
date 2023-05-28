71e3a0d3fcdcea5be4ef48189b49c0471c94cb4e
dwiki-mukti <243dwiki.com@gmail.com>
2023-05-28 19:32:02 +0700

    Design Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,94 @@ import { CSSProperties } from "react"
 
 export default function Home() {
   return (
-    <div className="container mx-auto p-4">
-      <MyMap
-        style={{ width: "100%", height: '20rem', borderRadius: '1rem' }}
-      />
+    <div className="container mx-auto p-4 min-h-screen">
+      <div className="p-4 text-center capitalize border-b">
+        <div className="text-xl font-semibold">crash monit</div>
+        <div className="">monitor daerah rawan kecelakaan</div>
+      </div>
+      <div className="grid grid-cols-5 gap-8 p-8">
+        <div className="col-span-3">
+          <div className="max-h-[34rem] overflow-auto">
+            <table className="w-full">
+              <thead>
+                <tr>
+                  <th className="px-3 pb-3">No.</th>
+                  <th className="px-3 pb-3">Kronologi</th>
+                  <th className="px-3 pb-3">Koordinat</th>
+                  <th className="px-3 pb-3">Hapus</th>
+                </tr>
+              </thead>
+              <tbody>
+                {(() => {
+                  const ret = [];
+                  for (let i = 0; i < 10; i++) {
+                    ret.push(
+                      <tr className="border-t cursor-pointer hover:bg-gray-100">
+                        <td className="p-3 text-center">{i + 1}</td>
+                        <td className="p-3 ">Kecelakaan tunggal, klutuk terjungkir</td>
+                        <td className="p-3 text-center">11.2029, 21.0199</td>
+                        <td className="p-3 flex">
+                          <div className="flex m-auto rounded-full w-[1.75rem] aspect-square text-red-500 hover:bg-red-500 hover:text-white">
+                            <a className="m-auto font-bold text-xs">X</a>
+                          </div>
+                        </td>
+                      </tr>
+                    )
+                  }
+                  return ret
+                })()}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="col-span-2">
+          <MyMap
+            style={{ width: "100%", height: '20rem', borderRadius: '1rem' }}
+          />
+          <form>
+            <div className="grid grid-cols-2 gap-4 mt-2">
+              <div className="mt-6 relative rounded-lg h-[2.5rem]">
+                <label
+                  htmlFor="lat"
+                  className="capitalize text-gray-600 text-[12px] absolute mt-[-10px] ml-[7px] px-[3px] bg-white"
+                >Titik lintang kecelakaan</label>
+                <input
+                  id="lat"
+                  name="lat"
+                  type="number"
+                  className="w-full h-full px-3 rounded-lg focus:outline-none border focus:border-sky-500"
+                />
+              </div>
+              <div className="mt-6 relative rounded-lg h-[2.5rem]">
+                <label
+                  htmlFor="long"
+                  className="capitalize text-gray-600 text-[12px] absolute mt-[-10px] ml-[7px] px-[3px] bg-white"
+                >Titik bujur kecelakaan</label>
+                <input
+                  id="long"
+                  name="long"
+                  type="number"
+                  className="w-full h-full px-3 rounded-lg focus:outline-none border focus:border-sky-500"
+                />
+              </div>
+            </div>
+            <div className="mt-6 relative rounded-lg h-[5rem]">
+              <label
+                htmlFor="long"
+                className="capitalize text-gray-600 text-[12px] absolute mt-[-10px] ml-[7px] px-[3px] bg-white"
+              >Kronologi kecelakaan</label>
+              <textarea
+                id="long"
+                name="long"
+                className="w-full h-full px-3 rounded-lg focus:outline-none border focus:border-sky-500"
+              ></textarea>
+            </div>
+            <div className="mt-6 flex">
+              <button className="text-xs rounded-lg bg-blue-500 text-white ml-auto px-6 pt-2 pb-[.7rem]">Simpan</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
