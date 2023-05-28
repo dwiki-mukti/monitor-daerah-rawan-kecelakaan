10fafe7bc3ae0b3b47e190d0cd3b6fc2760642d7
dwiki-mukti <243dwiki.com@gmail.com>
2023-05-28 20:34:11 +0700

    build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { clusterCountLayer, clusterLayer, unclusteredPointLayer } from "@/utils/dataset/clusterMapBox"
-import { Layer, Map, Source } from "react-map-gl"
-import { CSSProperties } from "react"
+import { Layer, Map, Popup, Source } from "react-map-gl"
+import { CSSProperties, useState } from "react"
 
 
 export default function Home() {
@@ -12,94 +12,28 @@ export default function Home() {
       </div>
       <div className="grid grid-cols-5 gap-8 p-8">
         <div className="col-span-3">
-          <div className="max-h-[34rem] overflow-auto">
-            <table className="w-full">
-              <thead>
-                <tr>
-                  <th className="px-3 pb-3">No.</th>
-                  <th className="px-3 pb-3">Kronologi</th>
-                  <th className="px-3 pb-3">Koordinat</th>
-                  <th className="px-3 pb-3">Hapus</th>
-                </tr>
-              </thead>
-              <tbody>
-                {(() => {
-                  const ret = [];
-                  for (let i = 0; i < 10; i++) {
-                    ret.push(
-                      <tr className="border-t cursor-pointer hover:bg-gray-100">
-                        <td className="p-3 text-center">{i + 1}</td>
-                        <td className="p-3 ">Kecelakaan tunggal, klutuk terjungkir</td>
-                        <td className="p-3 text-center">11.2029, 21.0199</td>
-                        <td className="p-3 flex">
-                          <div className="flex m-auto rounded-full w-[1.75rem] aspect-square text-red-500 hover:bg-red-500 hover:text-white">
-                            <a className="m-auto font-bold text-xs">X</a>
-                          </div>
-                        </td>
-                      </tr>
-                    )
-                  }
-                  return ret
-                })()}
-              </tbody>
-            </table>
-          </div>
+          <TableIncident />
         </div>
         <div className="col-span-2">
           <MyMap
-            style={{ width: "100%", height: '20rem', borderRadius: '1rem' }}
+            style={{ width: "100%", height: '20rem', borderRadius: '1rem', overflow: "hidden" }}
           />
-          <form>
-            <div className="grid grid-cols-2 gap-4 mt-2">
-              <div className="mt-6 relative rounded-lg h-[2.5rem]">
-                <label
-                  htmlFor="lat"
-                  className="capitalize text-gray-600 text-[12px] absolute mt-[-10px] ml-[7px] px-[3px] bg-white"
-                >Titik lintang kecelakaan</label>
-                <input
-                  id="lat"
-                  name="lat"
-                  type="number"
-                  className="w-full h-full px-3 rounded-lg focus:outline-none border focus:border-sky-500"
-                />
-              </div>
-              <div className="mt-6 relative rounded-lg h-[2.5rem]">
-                <label
-                  htmlFor="long"
-                  className="capitalize text-gray-600 text-[12px] absolute mt-[-10px] ml-[7px] px-[3px] bg-white"
-                >Titik bujur kecelakaan</label>
-                <input
-                  id="long"
-                  name="long"
-                  type="number"
-                  className="w-full h-full px-3 rounded-lg focus:outline-none border focus:border-sky-500"
-                />
-              </div>
-            </div>
-            <div className="mt-6 relative rounded-lg h-[5rem]">
-              <label
-                htmlFor="long"
-                className="capitalize text-gray-600 text-[12px] absolute mt-[-10px] ml-[7px] px-[3px] bg-white"
-              >Kronologi kecelakaan</label>
-              <textarea
-                id="long"
-                name="long"
-                className="w-full h-full px-3 rounded-lg focus:outline-none border focus:border-sky-500"
-              ></textarea>
-            </div>
-            <div className="mt-6 flex">
-              <button className="text-xs rounded-lg bg-blue-500 text-white ml-auto px-6 pt-2 pb-[.7rem]">Simpan</button>
-            </div>
-          </form>
+          <FormIncident />
         </div>
       </div>
     </div>
   )
 }
 
-
+interface typePopupIncident {
+  id: string | number,
+  latitude: number,
+  longitude: number,
+  chronology: string
+}
 
 function MyMap({ style }: { style?: CSSProperties }) {
+  const [PopupIncident, setPopupIncident] = useState<typePopupIncident | null>(null)
 
   return (
     <Map
@@ -149,18 +83,18 @@ function MyMap({ style }: { style?: CSSProperties }) {
         // event click uncluster/micro point
         map.on('click', 'unclustered-point', (e) => {
           const feature: any = e.features?.[0];
-          const coordinates = feature.geometry.coordinates.slice();
+          const coordinates = feature.geometry.coordinates;
 
           while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
             coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
           }
 
-          new mapboxgl.Popup()
-            .setLngLat(coordinates)
-            .setHTML(
-              `${feature.properties.title}`
-            )
-            .addTo(map);
+          setPopupIncident({
+            id: feature.properties.id,
+            longitude: coordinates[0],
+            latitude: coordinates[1],
+            chronology: feature.properties.chronology
+          })
         });
 
       }}
@@ -176,7 +110,104 @@ function MyMap({ style }: { style?: CSSProperties }) {
         <Layer {...clusterLayer} />
         <Layer {...clusterCountLayer} />
         <Layer {...unclusteredPointLayer} />
+        {PopupIncident && (
+          <Popup
+            longitude={PopupIncident.longitude}
+            latitude={PopupIncident.latitude}
+            onClose={() => setPopupIncident(null)}
+          >
+            {PopupIncident.chronology}
+          </Popup>
+        )}
       </Source>
-    </Map>
+    </Map >
+  )
+}
+
+
+
+
+function FormIncident() {
+  return (
+    <form>
+      <div className="grid grid-cols-2 gap-4 mt-2">
+        <div className="mt-6 relative rounded-lg h-[2.5rem]">
+          <label
+            htmlFor="lat"
+            className="capitalize text-gray-600 text-[12px] absolute mt-[-10px] ml-[7px] px-[3px] bg-white"
+          >Titik lintang kecelakaan</label>
+          <input
+            id="lat"
+            name="lat"
+            type="number"
+            className="w-full h-full px-3 rounded-lg focus:outline-none border focus:border-sky-500"
+          />
+        </div>
+        <div className="mt-6 relative rounded-lg h-[2.5rem]">
+          <label
+            htmlFor="long"
+            className="capitalize text-gray-600 text-[12px] absolute mt-[-10px] ml-[7px] px-[3px] bg-white"
+          >Titik bujur kecelakaan</label>
+          <input
+            id="long"
+            name="long"
+            type="number"
+            className="w-full h-full px-3 rounded-lg focus:outline-none border focus:border-sky-500"
+          />
+        </div>
+      </div>
+      <div className="mt-6 relative rounded-lg h-[5rem]">
+        <label
+          htmlFor="long"
+          className="capitalize text-gray-600 text-[12px] absolute mt-[-10px] ml-[7px] px-[3px] bg-white"
+        >Kronologi kecelakaan</label>
+        <textarea
+          id="long"
+          name="long"
+          className="w-full h-full px-3 rounded-lg focus:outline-none border focus:border-sky-500"
+        ></textarea>
+      </div>
+      <div className="mt-6 flex">
+        <button className="text-xs rounded-lg bg-blue-500 text-white ml-auto px-6 pt-2 pb-[.7rem]">Simpan</button>
+      </div>
+    </form>
+  )
+}
+
+
+function TableIncident() {
+  return (
+    <div className="max-h-[34rem] overflow-auto">
+      <table className="w-full">
+        <thead>
+          <tr>
+            <th className="px-3 pb-3">No.</th>
+            <th className="px-3 pb-3">Kronologi</th>
+            <th className="px-3 pb-3">Koordinat</th>
+            <th className="px-3 pb-3">Hapus</th>
+          </tr>
+        </thead>
+        <tbody>
+          {(() => {
+            const ret = [];
+            for (let i = 0; i < 10; i++) {
+              ret.push(
+                <tr key={i} className="border-t cursor-pointer hover:bg-gray-100">
+                  <td className="p-3 text-center">{i + 1}</td>
+                  <td className="p-3 ">Kecelakaan tunggal, klutuk terjungkir</td>
+                  <td className="p-3 text-center">11.2029, 21.0199</td>
+                  <td className="p-3 flex">
+                    <div className="flex m-auto rounded-full w-[1.75rem] aspect-square text-red-500 hover:bg-red-500 hover:text-white">
+                      <a className="m-auto font-bold text-xs">X</a>
+                    </div>
+                  </td>
+                </tr>
+              )
+            }
+            return ret
+          })()}
+        </tbody>
+      </table>
+    </div>
   )
 }
