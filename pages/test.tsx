f4e6d28e76465c6fe5a852cad0343d8d9ec3dc98
dwiki-mukti <243dwiki.com@gmail.com>
2023-05-29 17:12:17 +0700

    Test

--- pages/test.tsx
+++ pages/test.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Test() {
     return (
-        <div>Test</div>
+        <>
+            <div>NEXT_PUBLIC_API_URL: {process.env.NEXT_PUBLIC_API_URL}</div>
+            <div>NEXT_PUBLIC_GMAPS_API_KEY: {process.env.NEXT_PUBLIC_GMAPS_API_KEY}</div>
+        </>
     )
 }
